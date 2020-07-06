448d8b1c9d627d8ef1d317c0078f6a5b63611e38
Ilia Nemankov <INemankov@luxoft.com>
2020-07-06 10:31:52 +0200

    Convert xml report from all platform in one json summary report

--- build_report.py
+++ build_report.py
@@ -4,6 +4,9 @@ import logging
 import jinja2
 from shutil import copytree
 from config import *
+from glob import glob
+import xmltodict
+import json
 
 
 logging.basicConfig(filename='report_building.log', level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(message)s')
@@ -16,6 +19,11 @@ def parse_args():
     parser.add_argument('--output_dir', required=True)
 
     return parser.parse_args()
+
+
+def save_json_report(report, output_dir, file_name):
+    with open(os.path.abspath(os.path.join(output_dir, file_name)), "w", encoding='utf8') as file:
+        json.dump(report, file, indent=4, sort_keys=True)            
 
 
 def save_html_report(report, output_dir, file_name):
@@ -32,6 +40,17 @@ def prepare_results_dir(output_dir):
         logger.error('Fail during copying report resources: {}'.format(str(err)))
 
 
+def build_summary_template(test_results):
+    summary_report = {}
+    xml_reports = glob(os.path.join(test_results, XML_REPORT_PATTERN))
+    for xml_report_path in xml_reports:
+        with open(xml_report_path, 'r') as file:
+            xml_report = file.read()
+        platform_name = os.path.split(xml_report_path)[1].replace('Test-', '').split('.')[0]
+        summary_report[platform_name] = xmltodict.parse(xml_report)
+    return summary_report
+
+
 def main():
     args = parse_args()
     prepare_results_dir(args.output_dir)
@@ -40,6 +59,9 @@ def main():
         loader=jinja2.PackageLoader('build_report', 'report/templates'),
         autoescape=True
     )
+    summary_report = build_summary_template(args.test_results)
+    save_json_report(summary_report, args.output_dir, SUMMARY_REPORT_JSON)
+
     summary_template = env.get_template('summary_template.html')
     summary_html = summary_template.render(title='Results of RIF performance tests',
                                            text='some text')
--- config.py
+++ config.py
@@ -1,1 +1,4 @@
 SUMMARY_REPORT_HTML='summary_report.html'
+SUMMARY_REPORT_JSON='summary_report.json'
+
+XML_REPORT_PATTERN='*.gtest.xml'
