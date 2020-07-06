b4e05dd6a3d14d7593cd2b88429b38ff236201eb
Ilia Nemankov <INemankov@luxoft.com>
2020-07-06 18:55:41 +0200

    Add generation of detailed reports

--- build_report.py
+++ build_report.py
@@ -43,7 +43,7 @@ def prepare_results_dir(output_dir):
         logger.error('Fail during copying report resources: {}'.format(str(err)))
 
 
-def build_summary_template(test_results):
+def build_summary_report(test_results):
     summary_report = {}
     summary_report['results'] = {}
     summary_report['summary'] = {
@@ -61,9 +61,12 @@ def build_summary_template(test_results):
         with open(xml_report_path, 'r') as file:
             xml_report = file.read()
         platform_name = os.path.split(xml_report_path)[1].replace('Test-', '').split('.')[0]
-        # make platform name more readable
-        platform_name = platform_name.replace('_', ' '). replace('-', ' (') + ')'
         summary_report['results'][platform_name] = xmltodict.parse(xml_report)
+
+        # make platform name more readable
+        displayable_platform_name = platform_name.replace('_', ' '). replace('-', ' (') + ')'
+        summary_report['results'][platform_name]['name'] = displayable_platform_name
+
         summary_report['summary']['statuses']['Passed'] += int(summary_report['results'][platform_name]['testsuites']['@tests']) \
         - int(summary_report['results'][platform_name]['testsuites']['@failures']) - int(summary_report['results'][platform_name]['testsuites']['@errors']) \
         - int(summary_report['results'][platform_name]['testsuites']['@disabled'])
@@ -76,6 +79,14 @@ def build_summary_template(test_results):
     return summary_report
 
 
+def build_detailed_reports(env, summary_report, output_dir):
+    detailed_summary_template = env.get_template('detailed_summary_template.html')
+    for platform in summary_report['results']:
+        detailed_summary_html = detailed_summary_template.render(title='Results of RIF performance tests ({})'.format(summary_report['results'][platform]['name']),
+                                           report=summary_report['results'][platform])
+        save_html_report(detailed_summary_html, output_dir, platform + '_detailed.html')
+
+
 def main():
     args = parse_args()
     prepare_results_dir(args.output_dir)
@@ -84,13 +95,16 @@ def main():
         loader=jinja2.PackageLoader('build_report', 'report/templates'),
         autoescape=True
     )
-    summary_report = build_summary_template(args.test_results)
+    summary_report = build_summary_report(args.test_results)
     save_json_report(summary_report, args.output_dir, SUMMARY_REPORT_JSON)
 
     summary_template = env.get_template('summary_template.html')
     summary_html = summary_template.render(title='Results of RIF performance tests',
                                            report=summary_report)
     save_html_report(summary_html, args.output_dir, SUMMARY_REPORT_HTML)
+
+
+    build_detailed_reports(env, summary_report, args.output_dir)
 
 
 if __name__ == '__main__':
--- report/resources/js/scripts.js
+++ report/resources/js/scripts.js
@@ -3,3 +3,21 @@ function timeFormatter(value, row, index, field) {
     time.setSeconds(value);
     return time.toISOString().substr(11, 8);
 }
+
+function statusFormatter(value, row, index, field) {
+    switch(value) {
+    	case 'completed':
+    		return 'Passed'
+    		break
+    	case 'failure':
+    		return 'Failures'
+    		break
+    	case 'error':
+    		return 'Errors'
+    		break
+    	case 'suppressed':
+    		return 'Disabled'
+    		break
+    }
+    return 'Unknown'
+}
